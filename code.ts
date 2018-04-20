2b04746f2245b7ddfad89abd58bea54001702542
user <smarthonhk@gmail.com>
2018-04-20 15:25:24 +0800

    add number returned

--- code.ts
+++ code.ts
@@ -1,6 +1,6 @@
 namespace smartPH {
 	let ph_value = ""
-	//% blockId="readPH" block="Read PH value"
+	//% blockId="readPH" block="Read PH value (string with decimal)"
 	//% weight=41
 	//% blockGap=7
     export function readPH(): string {
@@ -23,5 +23,22 @@ namespace smartPH {
 						return ph_value.substr(0, 2) + "." + ph_value.substr(2,  ph_value.length)
 				}
     }
+
+		//% blockId="readPH" block="Read PH value (number)"
+		//% weight=40
+		//% blockGap=7
+		export function readPhNumber(): number {
+			let sensorarray: number[] = []
+			for (let i = 0; i < 10; i++) {
+						sensorarray.push(pins.analogReadPin(AnalogPin.P0))
+						basic.pause(10)
+				}
+				sensorarray.sort((n1, n2) => n1 - n2);
+			for (let value of sensorarray) {
+						serial.writeLine(value.toString())
+				}
+				ph_value = sensorarray[5] * 5 * 10 * 35/ 1024
+				return ph_value;
+		}
 
 }
